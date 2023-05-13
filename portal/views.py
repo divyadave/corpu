14b6f1bdb4691726b20be3655084a2bab366e3a0
Pkm17395 <pavankmuppala.go@gmail.com>
2023-05-13 17:53:09 +1000

    unit page added

--- portal/views.py
+++ portal/views.py
@@ -5,11 +5,13 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import  login
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.template import loader
+from django.http import JsonResponse
+from .forms import UnitForm
 
 
 def TaskList(LoginRequiredMixin, request):
@@ -25,6 +27,7 @@ class CustomLogin(LoginView):
     def get_success_url(self):
         return reverse_lazy('detail')
 
+
 class CustomRegister(FormView):
     template_name = 'register.html'
     form_class = UserCreationForm
@@ -36,12 +39,31 @@ class CustomRegister(FormView):
         if user is not None:
             login(self.request, user)
         return super(CustomRegister, self).form_valid(form)
-    
+
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('detail')
-        return super(CustomRegister,self).get( *args, **kwargs)
+        return super(CustomRegister, self).get(*args, **kwargs)
+
 
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+
+def unit_page(request):
+    form = UnitForm()
+    return render(request, 'unit_page.html', {'form': form})
+
+
+def apply_view(request):
+    if request.method == 'POST':
+        form = UnitForm(request.POST)
+        if form.is_valid():
+            # Process the form data and perform desired actions
+            # ...
+
+            return render(request, 'success.html')
+    else:
+        form = UnitForm()
+    return render(request, 'apply.html', {'form': form})
