e76c28a4dfa1083c9dba3a463b0cf11de004c8e2
Dhruvi Trivedi <133548939+DhruviT1806@users.noreply.github.com>
2023-05-15 18:32:42 +1000

    registered userprofile to admin

--- portal/admin.py
+++ portal/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import PermanentStaff, SessionalStaff, Unit, SessionalApplication, Availibility
+from .models import UserProfile, PermanentStaff, SessionalStaff, Unit, SessionalApplication, Availibility
 
 # Define admin view for models
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
 
 class PermanentStaffAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'qualifications', 'availability')
@@ -19,6 +21,7 @@ class AvailabilityAdmin(admin.ModelAdmin):
     list_display = ("casual_id","day_of_the_week","start_time","end_time",)
 
 # Register your models here.
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(PermanentStaff, PermanentStaffAdmin)
 admin.site.register(SessionalStaff,SessionalStaffAdmin)
 admin.site.register(SessionalApplication, SessionalApplicationAdmin)
