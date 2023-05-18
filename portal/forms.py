6ac1eae5020df64336fafb2df17fa4961e4e3c7e
Dhruvi Trivedi <133548939+DhruviT1806@users.noreply.github.com>
2023-05-18 19:37:38 +1000

    added helper and layout in forms.py

--- portal/forms.py
+++ portal/forms.py
@@ -3,6 +3,9 @@ from .models import Unit
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from crispy_forms.helper import FormHelper 
+from crispy_forms.layout import Submit 
+
 class UnitForm(forms.ModelForm):
     class Meta:
         model = Unit
@@ -21,3 +24,18 @@ class PermanentStaffUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
+
+class LoginForm(forms.Form): 
+    username = forms.CharField(label="Username") 
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password") 
+
+def __init__(self, *args, **kwargs): 
+    super(LoginForm, self).__init__(*args, **kwargs) 
+    self.helper = FormHelper() 
+    self.helper.form_method = 'post' 
+    self.helper.add_input(Submit('submit', 'Login')) 
+
+def clean(self): 
+    cleaned_data = super(LoginForm, self).clean() 
+    username = cleaned_data.get('username') 
+    password = cleaned_data.get('password') # Add validation as needed return cleaned_data
