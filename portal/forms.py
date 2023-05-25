064a77bd633ce2802e503db1067f8871222372f0
Pkm17395 <pavankmuppala.go@gmail.com>
2023-05-25 11:37:29 +1000

    register page edited and working

--- portal/forms.py
+++ portal/forms.py
@@ -3,47 +3,54 @@ from .models import Unit
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-from crispy_forms.helper import FormHelper 
-from crispy_forms.layout import Submit 
+# from crispy_forms.helper import FormHelper
+# from crispy_forms.layout import Submit
 
 from .models import JobListing
 from .models import CreateJob
 
+
 class CreateJobForm(forms.ModelForm):
     class Meta:
         model = CreateJob
-        fields = ['unit', 'course_description', 'required_qualification', 'teaching_materials', 'session_times', 'responsibilities', 'benefits']
+        fields = ['unit', 'course_description', 'required_qualification',
+                  'teaching_materials', 'session_times', 'responsibilities', 'benefits']
+
 
 class UnitForm(forms.Form):
     class Meta:
         model = Unit
         # fields = ['unitName', 'courseDescription', 'requiredQualification', 'teachingMaterials',
         #          'sessionTimes', 'lecturer', 'lecturerEmail']
-        fields  = ['UnitName','CourseDescription','RequiredQualification']
+        fields = ['UnitName', 'CourseDescription', 'RequiredQualification']
+
 
 class RegistrationForm(UserCreationForm):
-  
-
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
 
 class PermanentStaffUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
 
-class LoginForm(forms.Form): 
-    username = forms.CharField(label="Username") 
-    password = forms.CharField(widget=forms.PasswordInput(), label="Password") 
 
-def __init__(self, *args, **kwargs): 
-    super(LoginForm, self).__init__(*args, **kwargs) 
-    self.helper = FormHelper() 
-    self.helper.form_method = 'post' 
-    self.helper.add_input(Submit('submit', 'Login')) 
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Username")
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
 
-def clean(self): 
-    cleaned_data = super(LoginForm, self).clean() 
-    username = cleaned_data.get('username') 
-    password = cleaned_data.get('password') # Add validation as needed return cleaned_data
+
+def __init__(self, *args, **kwargs):
+    super(LoginForm, self).__init__(*args, **kwargs)
+    self.helper = FormHelper()
+    self.helper.form_method = 'post'
+    self.helper.add_input(Submit('submit', 'Login'))
+
+
+def clean(self):
+    cleaned_data = super(LoginForm, self).clean()
+    username = cleaned_data.get('username')
+    # Add validation as needed return cleaned_data
+    password = cleaned_data.get('password')
--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import CustomLogin, CustomRegister, main, sessionalProfile, unit_page, apply_view, dashboard, sessionalapplicant, viewapplication, createjob, joblisting, viewapplication, listofapplications, viewjob, listofjobs, applicantjobdetail, about, contact
+from .views import CustomLogin, CustomRegister, main, sessionalProfile, unit_page, apply_view, dashboard, sessionalapplicant, viewapplication, createjob, joblisting, viewapplication, listofapplications, viewjob, listofjobs, applicantjobdetail
 
 urlpatterns = [
     path('', main, name="home"),
@@ -21,4 +22,11 @@ urlpatterns = [
     path('view/', applicantjobdetail, name='applicantjob'),
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
+    path('createjob/', createjob, name='createjob'),
+    path('sessionalapplicant/', sessionalapplicant, name='sessionalapplicant'),
+    path('joblisting/', joblisting, name='joblisting'),
+    path('listofapplications/', listofapplications, name='listofapplications'),
+    path('viewjob/', viewjob, name='viewjob'),
+    path('listofjobs/', listofjobs, name='listofjobs'),
+    path('view/', applicantjobdetail, name='applicantjob'),
 ]
